c5998742c4fe2668190dbc6fe64ae3db92571e42
Filip Kosecek <kosecek@proton.me>
2026-04-03 18:09:44 +0200

    ws2812.c: Make the code more readable.

--- src/ws2812.c
+++ src/ws2812.c
@@ -6,20 +6,20 @@
 #include "ws2812.h"
 #include "ws2812.pio.h"
 
+#ifdef RGBW
+#define IS_RGBW true
+#else
+#define IS_RGBW false
+#endif
+
 static uint32_t led_strip[N_LEDS] = {0};
 
 void ws2812_init(void)
 {
-#ifdef RGBW
-        static bool is_rgbw = true;
-#else
-        static bool is_rgbw = false;
-#endif
-
         uint offset;
 
         offset = pio_add_program(PIO_BANK, &ws2812_program);
-        ws2812_program_init(PIO_BANK, SM_INDEX, offset, WS2812_PIN, 800000, is_rgbw);
+        ws2812_program_init(PIO_BANK, SM_INDEX, offset, WS2812_PIN, 800000, IS_RGBW);
 }
 
 int ws2812_set_led(unsigned int i, uint8_t red, uint8_t green, uint8_t blue
@@ -31,17 +31,15 @@ int ws2812_set_led(unsigned int i, uint8_t red, uint8_t green, uint8_t blue
         if (i >= N_LEDS)
                 return -1;
 
+#ifdef RGBW
         led_strip[i] = (uint32_t) blue          |
                        ((uint32_t) red << 8)    |
-                       ((uint32_t) green << 16)
-#ifdef RGBW
-                       |
-                       ((uint32_t) white << 24)
-#endif
-                       ;
-
-#ifndef RGBW
-        led_strip[i] <<= 8u;
+                       ((uint32_t) green << 16) |
+                       ((uint32_t) white << 24);
+#else
+        led_strip[i] =  ((uint32_t) blue << 8) |
+                        ((uint32_t) red << 16) |
+                        ((uint32_t) green << 24);
 #endif
 
         return 0;
